70e9ec7773698e1f52f5511e7f09dbea3fb5c829
developer <coderdarkllc@gmail.com>
2026-06-27 13:56:31 -0500

    init

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import { createCliRenderer, TextAttributes } from "@opentui/core";
+import { useState, useRef } from "react";
+import { createCliRenderer, TextAttributes, InputRenderable } from "@opentui/core";
 import { createRoot } from "@opentui/react";
 import "dotenv/config";
 import OpenAI from "openai";
@@ -10,17 +10,33 @@ const openai = new OpenAI({
 });
 
 function App() {
-  const [input, setInput] = useState("");
+  const inputRef = useRef<InputRenderable>(null);
+  const [context, setContext] = useState<{ role: string; content: string }[]>([
+    { role: "system", content: "You are a helpful assistant." },
+  ]);
+
+  function handleInputChange(value: string) {
+    setContext([...context, { role: "user", content: value }]);
+
+    if (inputRef.current) {
+      inputRef.current.value = "";
+    }
+  }
 
   return (
-    <box flexDirection="column" alignItems="flex-start" gap={2}>
-      <box alignItems="flex-start" gap={1}>
+    <box flexDirection="column" alignItems="flex-start" gap={1} height="100%">
+      <box alignItems="flex-start" width="100%" height="20%" padding={1} gap={1} backgroundColor="orange">
         <ascii-font font="tiny" text="OpenSLM" />
         <text>Your open small language model cli</text>
       </box>
-      <text>{input}</text>
-      <box justifyContent="center" width="100%">
-      <input value={input} onChange={setInput} placeholder="Ask me anything..." />
+      <box width="100%" backgroundColor="gray" height={"75%"} padding={1}>
+        <text>{context.map((item) => item.content).join("\n")}</text>
+      </box>
+      <box flexDirection="row" width="100%" height="5%">
+        <text>{'>'} </text>
+        <box width="100%">
+          <input ref={inputRef} onChange={handleInputChange} placeholder="Ask me anything..." />
+        </box>
       </box>
     </box>
   );
